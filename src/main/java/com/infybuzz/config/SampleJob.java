c48460c6816ced50cdf53e22a26dddd673976335
boonk <boonk@local.mail.com>
2022-07-25 21:35:11 +0700

    19-Make job param unique

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -41,6 +42,7 @@ public class SampleJob {
 		logger.info("1 Job");
 
 		return jobBuilderFactory.get("First Job")
+				.incrementer(new RunIdIncrementer())
 				.start(firstStep())// Read all tasklet in firstStep
 				.next(secondStep())// Read all tasklet in secondStep
 				.build(); // Execute Tasklet
